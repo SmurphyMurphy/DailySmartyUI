335456629158daa74db16b388c402cda6004178c
Justin Murphy <murphyjustin860@gmail.com>
2022-12-26 08:56:33 -0700

    created recent posts and search bar components and used them in the home component

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="search DailySmarty" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
