1de87b7cc6975516add6fb4864cb69b14e2bdd06
Justin Murphy <murphyjustin860@gmail.com>
2023-01-03 18:38:52 -0700

    modified fetchpostswithquery actionCreator to take in an optional callback

--- src/components/home.js
+++ src/components/home.js
@@ -9,8 +9,9 @@ import * as actions from "../actions";
 
 class Home extends Component {
   handleSearchbarSubmit(query) {
-    this.props.fetchPostsWithQuery(query);
-    this.props.history.push("/results");
+    this.props.fetchPostsWithQuery(query, () => {
+      this.props.history.push("/results");
+    });
   }
 
   render() {
