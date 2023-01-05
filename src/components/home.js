d04eba13a32f25fe2c9684c691523e85a371667c
Justin Murphy <murphyjustin860@gmail.com>
2023-01-04 19:08:39 -0700

    began styling the home component and talked about CSS Grid in codepen

--- src/components/home.js
+++ src/components/home.js
@@ -16,7 +16,7 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div className="home">
         <Logo />
         <SearchBar onSubmit={(query) => this.handleSearchbarSubmit(query)} />
         <RecentPosts />
